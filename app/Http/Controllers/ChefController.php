6f989085f7cdc6ed6f30679767aac9c498d177e0
Bhavin Solanki <bhavin@logicrays.com>
2021-02-01 18:52:36 +0530

    Debug order list API

--- app/Http/Controllers/ChefController.php
+++ app/Http/Controllers/ChefController.php
@@ -779,6 +779,10 @@ class ChefController extends Controller
 
     public function orderlist(Request $request){
 
+        echo 'orderlist';
+        exit();
+
+
         $user = User::where(['api_token' => $request->api_token])->first();
         if($user){
 
